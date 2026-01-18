1c2e85829c2bd8a57af391ef242d65b2d737b37c
WookiesRpeople2 <keentobiansky@gmail.com>
2026-01-18 18:06:41 +0100

    added string macro

--- errors.h
+++ errors.h
@@ -5,13 +5,13 @@
 
 #define POSTGRES_CONNECTION_ERROR "An error occured while trying to connect to the database %s"
 
-#define PRINT_ERROR_DO_ACTION(msg, action) do { \
-  fprintf(stderr, "%s\n", msg);                 \
-  action;                                       \
-} while(0)
+#define PRINT_ERROR_DO_ACTION(msg, action, ...)     \
+  do {                                              \
+      fprintf(stderr, msg "\n", ##__VA_ARGS__);     \
+      action;                                       \
+  } while(0)
 
-#define ERROR_EXIT(msg) do {                      \
-  PRINT_ERROR_DO_ACTION(msg, exit(EXIT_FAILURE)); \
-} while(0)
+#define ERROR_EXIT(msg, ...) \
+    PRINT_ERROR_DO_ACTION(msg, exit(EXIT_FAILURE), ##__VA_ARGS__)
 
 #endif
