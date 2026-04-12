cf3348a7a6319138d3214c33f9b3b33d324d6033
WookiesRpeople2 <117142278+WookiesRpeople2@users.noreply.github.com>
2026-04-13 00:43:00 +0200

    added defines to object

--- object.h
+++ object.h
@@ -3,12 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct obj_val_t obj_val_t;
+typedef struct obj_t obj_t;
+
+typedef enum {
+  OBJ_STRING,
+  OBJ_INT,
+  OBJ_FLOAT,
+  OBJ_BOOL,
+  OBJ_ARRAY,
+  OBJ_OBJECT
+} obj_type_t;
+
+struct obj_val_t {
+  obj_type_t type;
+  union {
+    struct {
+      char *ptr;
+      size_t len;
+    } string;
+    int integer;
+    float floating;
+    int boolean;
+    struct {
+      obj_val_t *items;
+      size_t count;
+    } array;
+    struct {
+      obj_t *items;
+      size_t count;
+    } object;
+  };
+};
+
+struct obj_t {
   char *key;
-  char *value;
   size_t key_len;
-  size_t value_len;
-} obj_t;
+  obj_val_t value;
+};
 
 typedef struct {
   obj_t *items;
@@ -16,16 +48,47 @@ typedef struct {
   size_t capacity;
 } obj_list_t;
 
-#define obj_pair(k, v)                                                         \
-  (obj_t){.key = (k),                                                          \
-          .value = (v),                                                        \
-          .key_len = sizeof(k) - 1,                                            \
-          .value_len = sizeof(v) - 1}
+typedef struct {
+  obj_t *items;
+  size_t count;
+} obj_root_t;
 
-#define obj_list(...)                                                          \
-  &(obj_list_t){.items = (obj_t[]){__VA_ARGS__},                               \
-                .count = sizeof((obj_t[]){__VA_ARGS__}) / sizeof(obj_t),       \
-                .capacity = sizeof((obj_t[]){__VA_ARGS__}) / sizeof(obj_t)}
+#define obj_string(s)                                                          \
+  (obj_val_t) {                                                                \
+    .type = OBJ_STRING, .string = {.ptr = (s), .len = sizeof(s) - 1}           \
+  }
+
+#define obj_int(n)                                                             \
+  (obj_val_t) { .type = OBJ_INT, .integer = (n) }
+
+#define obj_float(n)                                                           \
+  (obj_val_t) { .type = OBJ_FLOAT, .floating = (n) }
+
+#define obj_bool(b)                                                            \
+  (obj_val_t) { .type = OBJ_BOOL, .boolean = (b) }
+
+#define obj_array(...)                                                         \
+  (obj_val_t) {                                                                \
+    .type = OBJ_ARRAY, .array = {                                              \
+      .items = (obj_val_t[]){__VA_ARGS__},                                     \
+      .count = sizeof((obj_val_t[]){__VA_ARGS__}) / sizeof(obj_val_t)          \
+    }                                                                          \
+  }
+
+#define obj_object(...)                                                        \
+  (obj_val_t) {                                                                \
+    .type = OBJ_OBJECT, .object = {                                            \
+      .items = (obj_t[]){__VA_ARGS__},                                         \
+      .count = sizeof((obj_t[]){__VA_ARGS__}) / sizeof(obj_t)                  \
+    }                                                                          \
+  }
+
+#define obj_pair(k, v)                                                         \
+  (obj_t) { .key = (k), .key_len = sizeof(k) - 1, .value = (v) }
+
+#define obj_root(...)                                                          \
+  &(obj_root_t){.items = (obj_t[]){__VA_ARGS__},                               \
+                .count = sizeof((obj_t[]){__VA_ARGS__}) / sizeof(obj_t)}
 
 #define obj_get(list, k) obj_get((list)->items, (list)->count, (k))
 
@@ -35,7 +98,6 @@ typedef struct {
     for (size_t i = 0; i < (list)->count; i++) {                               \
       if (strcmp((list)->items[i].key, (k)) == 0) {                            \
         (list)->items[i].value = (v);                                          \
-        (list)->items[i].value_len = strlen(v);                                \
         found = 1;                                                             \
         break;                                                                 \
       }                                                                        \
@@ -46,10 +108,7 @@ typedef struct {
         (list)->items =                                                        \
             realloc((list)->items, (list)->capacity * sizeof(obj_t));          \
       }                                                                        \
-      (list)->items[(list)->count++] = (obj_t){.key = (k),                     \
-                                               .value = (v),                   \
-                                               .key_len = strlen(k),           \
-                                               .value_len = strlen(v)};        \
+      (list)->items[(list)->count++] = obj_pair(k, v);                         \
     }                                                                          \
   } while (0)
 
